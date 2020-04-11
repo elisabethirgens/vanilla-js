2fca80a79331b41e3c213b7c5ffdfbdbc484c054
Elisabeth Irgens <hello@elisabethirgens.com>
2020-04-11 20:59:19 +0200

    Add files from doing ex 34: Click Outside Modal 🐭

---
+++ modal.js
@@ -0,0 +1,50 @@
+// Grab the elements and store them in constants
+const cardButtons = document.querySelectorAll('.card button');
+const modalOuter = document.querySelector('.modal-backdrop');
+const modalInner = document.querySelector('.modal-inner');
+
+// Function declaration for a named function with parameter
+// This function creates the modals with content from data attributes
+function handleCardButtonClick(e) { // e for event (but could be named whatever)
+    const myButton = e.currentTarget; // property of Event interface
+    const myCard = myButton.closest('.card'); // like querySelect but upward in the DOM
+    const { pinyin } = myCard.dataset; // grabs the data-pinyin attribute
+    const { character } = myCard.dataset; // grabs the data-character attribute
+
+    // Change the content with Element.innerHTML that gets or sets HTML
+    //   but remember security considerations and XSS
+    modalInner.innerHTML = `
+    <div class="character">${character}</div>
+    <div class="pinyin">${pinyin}</div>
+  `; // That was a template literal with ${expression} placeholders
+
+    // Use the property Element.classList with method add()
+    modalOuter.classList.add('activated');
+}
+
+// and then the method remove() to modify the same DOMTokenList collection
+function closeModal() {
+    modalOuter.classList.remove('activated');
+}
+
+// Here is the functionality to click outside the modal
+// closeModal() is called only when a click is NOT registered in the closest modal
+modalOuter.addEventListener('click', function (e) {
+    const isOutside = !e.target.closest('.modal-inner');
+    if (isOutside) {
+        closeModal();
+    }
+});
+
+// Also listen for the event.key Escape to call the closeModal function
+window.addEventListener('keydown', event => {
+    if (event.key === 'Escape') {
+        closeModal();
+    }
+});
+
+// This method will execute a function for each array element
+//   and the array here is cardButtons holding all the buttons
+cardButtons.forEach(button =>
+    button.addEventListener('click', handleCardButtonClick)
+);
