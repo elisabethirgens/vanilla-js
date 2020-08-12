3aad2dc1ca185dd44af47e3b2d3aa4aaf323f5ca
Elisabeth Irgens <hello@elisabethirgens.com>
2020-08-12 17:32:40 +0200

    Write comments explaining clojures +++ for slider 🍪

--- slider.js
+++ slider.js
@@ -1,8 +1,10 @@
 function Slider(slider) {
+    // Check if an HTML element is passed into the function
     if (!(slider instanceof Element)) {
         throw new Error("Nope, no slider here!");
     }
 
+    // Declare empty block-scoped local variables inside this closure
     let prev;
     let current;
     let next;
@@ -12,6 +14,7 @@ function Slider(slider) {
     const nextButton = slider.querySelector(".flipNext");
 
     function startSlider() {
+        // Update value of the already declared variables to their elements
         current = slider.querySelector(".current") || slides.firstElementChild;
         prev = current.previousElementSibling || slides.lastElementChild;
         next = current.nextElementSibling || slides.firstElementChild;
@@ -23,12 +26,16 @@ function Slider(slider) {
         next.classList.add("next");
     }
 
+    // Declare function with the parameter it needs
     function move(direction) {
         const classesToRemove = ["prev", "current", "next"];
+        // Spread that array into these remove() methods
         prev.classList.remove(...classesToRemove);
         current.classList.remove(...classesToRemove);
         next.classList.remove(...classesToRemove);
+        // The function call within button listeners can have a direction
         if (direction === "back") {
+            // Use destructuring to reassign variables
             [prev, current, next] = [
                 prev.previousElementSibling || slides.lastElementChild,
                 prev,
@@ -36,20 +43,25 @@ function Slider(slider) {
             ];
         } else {
             [prev, current, next] = [
-                current,
-                next,
+                current, // Reassign current to prev
+                next, // Reassign next to current
+                // Reassign next to next OR if last slide use first 😅
                 next.nextElementSibling || slides.firstElementChild,
             ];
         }
+        // Re-run the function to shift classes after the move function
         applyClasses();
     }
 
+    // Call these functions inside the closure 🎉
     startSlider();
     applyClasses();
 
+    // Use arrow function and pass argument for direction back
     prevButton.addEventListener("click", () => move("back"));
     nextButton.addEventListener("click", move);
 }
 
+// Call function and pass it the ID from slider HTML
 Slider(document.querySelector("#show-food"));
 Slider(document.querySelector("#show-drinks"));
